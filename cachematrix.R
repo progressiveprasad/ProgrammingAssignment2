ff2e7150312b4325372865ca19dd56c6b001a3d5
progressiveprasad <progressive.prasad@gmail.com>
2017-05-28 18:10:07 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions here, makeCacheMatrix() which creates a "special" matrix object
+## which can cache its inverse and second one, cacheSolve() calculates the inverse of this "special" matrix
+## using R's solve() function. If the inverse has already been calculated and the contents
+## of the original matrix reamaining unchanged, then the cached matrix is retrieved and the message
+## "getting cached matrix" is displayed. This is achieved the <<- operator which keeps
+## looking up parent environments to find the name/variable
 
-## Write a short comment describing this function
+## makeCacheMatrix() creates a "special" object. It contains a list which sets and gets
+## the value of the matrix and its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+minv <- NULL
+set <- function(y){
+  x <<- y
+  minv <<- NULL
+}
+get <- function() x
+setInv <- function(inversem) minv <<- inversem
+getInv <- function() minv
+list (set = set, get = get,
+      setInv = setInv,
+      getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() calculates the inverse of the matrix created using makeCacheMatrix()
+## if the matrix is already created and unchanged, then the cached matrix is retrieved.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  minv <- x$getInv()
+  if(!is.null(minv)){
+    message("getting cached matrix")
+    return(minv)
+}
+matrix1 <- x$get()
+minv <- solve(matrix1, ...)
+x$setInv(minv)
+minv
 }
