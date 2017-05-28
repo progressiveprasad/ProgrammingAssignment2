ca539742ccc6c64944c1653d8ed8794fae085a0b
progressiveprasad <progressive.prasad@gmail.com>
2017-05-28 18:34:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
 ## There are two functions here, makeCacheMatrix() which creates a "special" matrix object
 ## which can cache its inverse and second one, cacheSolve() calculates the inverse of this "special" matrix
 ## using R's solve() function. If the inverse has already been calculated and the contents
-## of the original matrix reamaining unchanged, then the cached matrix is retrieved and the message
-## "getting cached matrix" is displayed. This is achieved the <<- operator which keeps
+## of the original matrix reamain unchanged, then the cached matrix is retrieved and the message
+## "getting cached matrix" is displayed. This is achieved using the <<- operator which keeps
 ## looking up parent environments to find the name/variable
 
 ## makeCacheMatrix() creates a "special" object. It contains a list which sets and gets
